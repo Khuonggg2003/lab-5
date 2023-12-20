0fa115288703cb31aee11a8d4313a5cbbd8ae520
Khuonggg2003 <anhkhuongg@gmail.com>
2023-12-20 16:13:12 +0700

    no message

---
+++ source_code/Core/Src/software_timer.c
@@ -0,0 +1,20 @@
+
+
+#include "software_timer.h"
+int timer0_counter = 0;
+int timer0_flag = 0;
+int TIMER_CYCLE = 10;
+
+void setTimer0 ( int duration ) {
+ timer0_counter = duration;
+ timer0_flag = 0;
+ }
+
+ void timer_run () {
+ if( timer0_counter > 0) {
+ timer0_counter --;
+ if( timer0_counter == 0) timer0_flag = 1;
+ }
+}
+
+
